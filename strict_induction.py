e6f67f9b40e5bbc89f2ed5913a7b0df9283b52ad
Ethan Hadley <ekhadley@gmail.com>
2024-07-31 15:49:31 -0500

    renaming, ipynb

--- strict_induction.py
+++ strict_induction.py
@@ -88,18 +88,13 @@ print(lime, f"top previous token heads: {top_pheads[:5]}", endc)
 print(cyan, f"top induction heads: {top_iheads[:5]}", endc)
 #%%
 patterns = t.stack([cache["pattern", layer] for layer in range(model.cfg.n_layers)])[:, 0, :, :]
-for layer in range(model.cfg.n_layers):
-    pattern = patterns[layer]
-    cv.attention.attention_patterns(
-        attention = pattern,
-        tokens = model.to_str_tokens(tokens[0]),
-        attention_head_names = [f"{layer}.{head}" for head in range(model.cfg.n_heads)],
-    )
-# our strongest prev token head is 4.11 with over 90% of its attn on the prev token. Second most has less than 50%.
-# our induction-y heads on the repeated tokens task were 5.1, 7.2, 5.0, 6.9, 5.5, decreasing monotonically with 5.1 at about .37 iscore
-# The fact that there are no standout inductiony heads lends credence to the idea that they are __literally__ just doing induction.
-# probably there is some specialization in the heads, like diff types of induction, or totally different, non induction jobs on
-# other prompts.
+layer = 4
+cv.attention.attention_patterns(
+    attention = patterns[layer],
+    tokens = model.to_str_tokens(tokens[0]),
+    attention_head_names = [f"{layer}.{head}" for head in range(model.cfg.n_heads)],
+)
+print("our strongest prev token head is 4.11 with over 90% of its attn on the prev token. Second most has less than 50%. our induction-y heads on the repeated tokens task were 5.1, 7.2, 5.0, 6.9, 5.5, decreasing monotonically with 5.1 at about .37 iscore The fact that there are no standout inductiony heads lends credence to the idea that they are __literally__ just doing induction. probably there is some specialization in the heads, like diff types of induction, or totally different, non induction jobs on other prompts.")
 
 #%%
 
@@ -116,8 +111,7 @@ def strict_prev_token_head_hook(
     patterns[..., 0, 0] = 1.0
     return patterns
 
-# Here we test our strict previous token head hook.
-# It provides a very small but consisten improvement to performance on the repeated random tokens task.
+print("Here we test our strict previous token head hook. It provides a very small but consistent improvement to performance on the repeated random tokens task.")
 tokens = gen_induction_test(16, 32)
 cleanlogits = model.forward(tokens)
 print(lime, f"original model score on the repeated random tokens task: {bold}{loss(cleanlogits, tokens)}{endc}", endc)
@@ -235,7 +229,7 @@ def random_ablation_map(model, tokens, start_layer=0, normalize=False):
     return scores
 
 #%%
-# pattern for 5.1 (an induction head) on harry potter
+print("pattern for 5.1 (an induction head) on harry potter")
 hptext = "Mr and Mrs Dursley, of number four, Privet Drive, were proud to say that they were perfectly normal, thank you very much. They were the last people you'd expect to be involved in anything strange or mysterious, because they just didn't hold with such nonsense. Mr Dursley was the director of a firm called Grunnings, which made drills."
 hptoks = model.tokenizer(hptext, return_tensors='pt', add_special_tokens=True)['input_ids'].to(device)
 _, hp_cache = model.run_with_cache(hptoks)
@@ -245,7 +239,7 @@ cv.attention.attention_pattern(
     tokens = str_tokens,
 )
 #%%
-# the pattern of a strict induction head on the same text
+print("the pattern of a strict induction head on the same text")
 model.reset_hooks()
 hook = partial(strict_induction_head_hook, head=5, tokens=hptoks, cache=hp_cache)
 #_, strict_hp_cache = model.run_with_hooks(hptoks, fwd_hooks=[("blocks.5.attn.hook_pattern", hook)])
@@ -255,13 +249,14 @@ cv.attention.attention_pattern(
     attention = strict_hp_cache["pattern", 5][0, 5],
     tokens = str_tokens,
 )
+model.reset_hooks()
 
 #%%
 #text = "Mr and Mrs Dursley, of number four, Privet Drive, were proud to say that they were perfectly normal, thank you very much. They were the last people you'd expect to be involved in anything strange or mysterious, because they just didn't hold with such nonsense. Mr Dursley was the director of a firm called Grunnings, which made drills. He was a big, beefy man with hardly any neck, although he did have a very large moustache. Mrs Dursley was thin and blonde and had nearly twice the usual amount of neck, which came in very useful as she spent so much of her time craning over garden fences, spying on the neighbours. The Dursleys had a small son called Dudley and in their opinion there was no finer boy anywhere. The Dursleys had everything they wanted, but they also had a secret, and their greatest fear was that somebody would discover it. They didn't think they could bear it if anyone found out about the Potters. Mrs Potter was Mrs Dursley's sister, but they hadn't met for several years; in fact, Mrs Dursley pretended she didn't have a sister, because her sister and her good- for-nothing husband were as unDursleyish as it was possible to be. The Dursleys shuddered to think what the neighbours would say if the Potters arrived in the street. The Dursleys knew that the Potters had a small son, too, but they had never even seen him. This boy was another good reason for keeping the Potters away; they didn't want Dudley mixing with a child like that."
 text = 'George Washington (February 22, 1732 - December 14, 1799) was an American Founding Father, military officer, and politician who served as the first president of the United States from 1789 to 1797. Appointed by the Second Continental Congress as commander of the Continental Army in 1775, Washington led Patriot forces to victory in the American Revolutionary War and then served as president of the Constitutional Convention in 1787, which drafted the current Constitution of the United States. Washington has thus become commonly known as the "Father of his Country".'
 toks = model.tokenizer(text, return_tensors='pt', add_special_tokens=True)['input_ids'].to(device)
 
-# here we test our strict induction head hook.
+print("here we test our strict induction head hook by observing loss differences on wikipedia text by ablating each head one at a time.")
 logits, strict_induction_cache = model.run_with_cache(toks)
 induction_ablation_map = strict_induction_ablation_map(model, strict_induction_cache, toks)
 imshow(induction_ablation_map, title="loss difference when ablating each head with a strict induction head")
