0c704d8012aea08c1c0ddc536616102d450edccf
Ethan Hadley <ekhadley@gmail.com>
2025-02-25 08:57:31 -0600

    ideas and stuff

--- algo_oct_23.py
+++ algo_oct_23.py
@@ -69,15 +69,15 @@ print(f"Mean probability on correct label: {probs_correct.mean():.3f}")
 print(f"Median probability on correct label: {probs_correct.median():.3f}")
 print(f"Min probability on correct label: {probs_correct.min():.3f}")
 #%%
-def show(batch_idx: int, dataset=dataset):
+def show(batch_idx: int, dataset=dataset, probs=True):
     logits: Tensor = model(dataset.toks)[:, dataset.list_len:-1, :]
-    logprobs = logits.log_softmax(-1) # [batch seq_len vocab_out]
-    probs = logprobs.softmax(-1)
+    if probs:
+        logits = logits.log_softmax(-1).softmax(-1)
 
     str_targets = dataset.str_toks[batch_idx][dataset.list_len+1: dataset.seq_len]
 
     imshow(
-        probs[batch_idx].T,
+        logits[batch_idx].T,
         y=dataset.vocab,
         x=[f"{dataset.str_toks[batch_idx][j]}<br><sub>({j})</sub>" for j in range(dataset.list_len+1, dataset.seq_len)],
         labels={"x": "Token", "y": "Vocab"},
@@ -108,17 +108,18 @@ def show_heads_on_input(patterns, tokens=None):
         attention_head_names=[f"head{i}.{j}" for i in range(n_layers) for j in range(n_heads)],
         tokens=toks,
     )
-def show_probs(logits: t.Tensor, toks: t.Tensor):
-    logprobs = logits.squeeze()[dataset.list_len:-1, :].log_softmax(-1) # [batch seq_len vocab_out]
-    probs = logprobs.softmax(-1)
+def show_logits(logits: t.Tensor, toks: t.Tensor, probs=True, title=None):
+    logits = logits.squeeze()[dataset.list_len:-1, :]
+    if probs:
+        logits = logits.log_softmax(-1).softmax(-1)
     strtoks = to_str_toks(toks)
     imshow(
-        probs.T,
+        logits.T,
         y=dataset.vocab,
         x=[f"{strtoks[i]}<br><sub>({i})</sub>" for i in range(11, 21)],
         labels={"x": "Token", "y": "Vocab"},
         xaxis_tickangle=0,
-        title=f"Sample model probabilities:<br>Unsorted = ({','.join(strtoks[:11])})",
+        title= (f"Sample model probabilities:<br>Unsorted = ({','.join(strtoks[:11])})" if title is None else title),
         text=[
             ["〇" if (str_tok == target) else "" for target in strtoks[11:]]
             for str_tok in dataset.vocab
@@ -160,17 +161,15 @@ def show_probs(logits: t.Tensor, toks: t.Tensor):
 # - We might see that the model gets confused when the next largest token is much larger than the current.
 #    - specifically since this algorithm mostly happens in the QK circuit, we should see that tokens will src from the correct next token less when the next token is much larger.
 # - If the sep token is being given a semantically low value like close to 0, we should see low tokens src from sep more than high ones
-# - We would expect the model to attend to all the post-sep tokens, as well as selectively attend to the pre-sep tokens based on their value and the current token's.
-# - we would expect that when the head attends to post-sep tokens, that the logits are all negative, becuase the only reason it would attend there is to inhibit duplicates.
-# - we would expect that when the head attends to pre-sep tokens, that the logits are all positive, because the only reason it would attend there is to boost the correct next token.
+# - We would expect the model to attend to all the post-sep tokens, as welblocks.0.hook_resid_post logits are all positive, because the only reason it would attend there is to boost the correct next token.
 # - If we take the embeddings and/or the Q, K values for all the tokens and dot them together ot get a similaaity matrix, we should expect the values to be proportional (?) to the distance to the main diagonal
 
 #%% attention patterns and logits on a sequence with a large jump
-jump_seq = [1, 2, 3, 4, 5, 45, 46, 47, 48, 49]
-random.shuffle(jump_seq)
+jump_seq = [0, 20, 25, 26, 27, 34, 46, 47, 48, 49]
+#random.shuffle(jump_seq)
 jump_seq_toks = list_to_toks(jump_seq)
 jump_seq_logits, jump_seq_cache = model.run_with_cache(jump_seq_toks)
-show_probs(jump_seq_logits, jump_seq_toks)
+show_logits(jump_seq_logits, jump_seq_toks)
 show_heads_on_input(jump_seq_cache['pattern', 0], jump_seq_toks)
 # The model does get confused when the sequence contains a large jump, for just the index of the jump.
 # After sep, h0.1 sources strongly from pre-sep tokens whose value is greater than the current one. It
@@ -184,21 +183,161 @@ show_heads_on_input(jump_seq_cache['pattern', 0], jump_seq_toks)
 # However, h0.1 also definitely attends more to tokens whcih are greater than the current, so its not just as simple
 # as 'source from the closest token'. The fact that 5 attends to sep so strongly is somewhat strange in light of this.
 
-# The pattern of h0.0 is quite different. attends strongly to 1 on sep and the first token of the sorted list. But from
-# tokens [2-5] is attends strongly to the current token, decaying as we go backwards in the sequence. So mostly on
-# the current token, some on the token behind, less on the token behind that.
+# So post-sep tokens appear to src from pre-sep tokens which are similair in numeric value, but especially so when
+# they are larger.
 
-# The patterns of h0.0 and h0.1 are visually quite different. After the sep token, H0.1 attends very little to post-sep tokens, whereas h0.0
-# attends there strongly.
-# H0.1 seems to be the 'what is next' head, and h0.0 seems to be the 'what isnt next' (as in what has already occurrsed in the sorted list) head.
-#%%
+#%% plotting dataset-avg attention pattern:
+
+avg_attn_pattern = cache['pattern', 0].mean(dim=0)
+cv.attention.attention_heads(
+    avg_attn_pattern,
+    attention_head_names=[f"head{i}.{j}" for i in range(n_layers) for j in range(n_heads)],
+    tokens=dataset.str_toks[0],
+)
+# Both heads' average attention value is uniform across the pre-sep tokens.
+# They mostly dont attend to post-sep tokens, with 2 exceptions. h0.0 strongly sources from early post-sep tokens,
+# And both heads src from the start of the sorted list a bit when the current tokenis the last in the sorted list.
+
+#%% sim matrix of all embeddings.
 
 embed_normed = model.W_E / model.W_E.norm(dim=-1, keepdim=True)
 imshow(einops.einsum(embed_normed, embed_normed, "voc1 d_model, voc2 d_model -> voc1 voc2"), title="Similarity matrix of embeddings")
 
+#%% now we will take the embeddings and mul with the keys and queries, and make a K-Q sim matrix for the whole dictionary.
+# Ahead of time predictions: Under the hypthesis that H0.1 srces from tokens which are near in value but larger than the
+# current token, the shouild expect the sim matrix to be low on and below the main diagonal, and the first few diagonals
+# above the main to have high value. (assuming we put keys on x axis and queries on y axis.)
+# I wont include position embeddings at first just for simplicity, I expect this will mess things up. Will repeat with
+# appropriate positional embeddings after.
+
+Q = einops.einsum(model.W_E, model.W_Q.squeeze(), "d_vocab d_model, n_heads d_model d_head -> d_vocab n_heads d_head")
+K = einops.einsum(model.W_E, model.W_K.squeeze(), "d_vocab d_model, n_heads d_model d_head -> d_vocab n_heads d_head")
+
+Q_normed = Q / Q.norm(dim=-1, keepdim=True)
+K_normed = K / K.norm(dim=-1, keepdim=True)
+
+sim_matrix = einops.einsum(Q_normed, K_normed, "vocq n_heads d_head, vock n_heads d_head -> n_heads vocq vock")
+imshow(sim_matrix[0], title="head 0.0 (W_E @ W_Q) @ (W_E @ W_K)^T (no positional embeddings)")
+imshow(sim_matrix[1], title="head 0.1 (W_E @ W_Q) @ (W_E @ W_K)^T (no positional embeddings)")
+
+# h0.1 looks like what i expected, with some extra structure i don't understand.
+
+# reminder: x is the key, y is the query.
+# sim[y][x] is tells us how much y wants to src from x.
+
+# H0.1 pattern description. The pattern has 4 quarters. The top left is the same as the bottom right, and the top right
+# is the same as the bottom left. Each quarter has a distinct pattern which splits it in half. The TL-BR quarters are all
+# uniform-ush and positive above (not on) the main diagonal of that quarter. The TR-BL quarters are all uniform-ish and
+# negative above the main diagonal of that quarter. the below-main-diagonal portion of all quartattend toers is basically 0.
+
+# Hypothesizing about h0.1: it seems that tokens will attend to tokens which are larger than themselves, but only so much
+# larger. This kind of makes sense: you dont usually want to attend to a much larger token than youraself, becuase there are
+# likely some other tokens that should come before it.
+# noticing confusion: why is it shaped like that though? shouldnt there be a gradual shift satarting from the main diagonal,
+# descreasing based on dest-src distance? Instead it has clear zones of attend, dont attend, and attend against.
+
+# This extra structure could be intended(?) for some reason I don't know, or it could be a result of the lack of positional
+# embeddings. If we try to use the h0.1 pattern to make an adversarial example, it basically doesnt work, so either the structure of the
+# patterns are not shortcomings, or if it is a shortcomings is is being accounted for.
+
+# h0.0 is less regular than h0.1 and is a bit harder to describe. It sort of looks like h0.1 except the below main diagonal
+# half of each quarter is filled in with the color of the quarter below it. So tl and br quarters are positive above the
+# main diagonal and negative below it. The tr and bl quarters are negative above the main diagonal and positive below it.
+# There are small 0 regions in between positive/negative sections, not a smooth gradient.
+
+#%% now we add a certain sequence position's positional embedding to the queries, and another position's to the keys.
+# This graph tells us how much does a y token at sequence position ypos, want to attend to an x token at xpos.
+
+# resummarizing working hypothesis: h0.1 is attending to pre-sep tokens which are near in value but larger than the
+# current token, incresing the odds of predicting that pre-sep token as being next. h0.0 is maybe just pushing down
+# the logits of the tokens we have already seen after the sep-token, avoiding repeats.
+
+# h0.0. predictions: we should see negative logits for any x, y when ypos and xpos are both after the sep token.
+# h0.1 predictions: when ypos is post-sep, we should see the same positive above diagonal pattern when xpos is 
+# pre-sep.
+
+# I'm actually unsure about h0.0 now. Why would we need a post-sep repeat suppressor? h0.1 can just not attend to
+# post sep tokens on its own, and only src values from pre-sep? If it can correctly src from only tokens
+# larger than the current, it should never attend to a pre-sep token which is already in the sorted list portion.
+
+Q_seq_pos = 11
+K_seq_pos = 11
+Q_plus_pos = einops.einsum(model.W_E + model.W_E_pos[Q_seq_pos], model.W_Q.squeeze(), "d_vocab d_model, n_heads d_model d_head -> d_vocab n_heads d_head")
+K_plus_pos = einops.einsum(model.W_E + model.W_E_pos[K_seq_pos], model.W_K.squeeze(), "d_vocab d_model, n_heads d_model d_head -> d_vocab n_heads d_head")
+
+Q_plus_pos_normed = Q_plus_pos / Q_plus_pos.norm(dim=-1, keepdim=True)
+K_plus_pos_normed = K_plus_pos / K_plus_pos.norm(dim=-1, keepdim=True)
+
+sim_matrix_with_pos = einops.einsum(Q_plus_pos_normed, K_plus_pos_normed, "vocq n_heads d_head, vock n_heads d_head -> n_heads vocq vock")
+imshow(sim_matrix_with_pos[0], title=f"head 0.0 K-Q sim matrix<br>(where Q has posembed for seq pos {Q_seq_pos} and K has seq pos {K_seq_pos})")
+imshow(sim_matrix_with_pos[1], title=f"head 0.1 K-Q sim matrix<br>(where Q has posembed for seq pos {Q_seq_pos} and K has seq pos {K_seq_pos})")
+
+# these patterns really make no sense to me.
+# they are generally positive ish below y = 38. There is a roundish positive patch for all pos values at 35, 35.
+# Is there something important about token 38? There are also visible lines at x=38 and y=38 in the previous sim
+# matrices, particularly for h0.0.
+
+# I suspect im doing something out of distribution. idk where though.
+# let's look at something other than attention patterns.
+
+#%% unembedding function
+
+def unembed(act, norm=True) -> t.Tensor:
+    if norm:
+        actmean = act.mean(dim=-1, keepdim=True)
+        actvar = act.var(dim=-1, keepdim=True)
+        act = (act - actmean) / t.sqrt(actvar + model.ln_final.eps)
+        #act = act * model.ln_final.w + model.ln_final.b
+    return einops.einsum(act, model.W_U, "... seq d_model, d_model vocab -> ... seq vocab") + model.b_U
+
+#%% unembedding various stuff
+# head output unembed predicitons. Assuming the heads are just boosting logits for repeating pre-sep tokens whcih are near in value but larger
+# than the current, we should see decaying positive logits, starting on the correct token and falling off as the tokens get larger.
+head = 0
+dataset_idx = 123
+show_logits(
+    unembed(cache["blocks.0.attn.hook_result"][dataset_idx, :, head]),
+    dataset.toks[dataset_idx],
+    probs=False,
+    title=f"h0.{head} output unembed on dataset_idx {dataset_idx}"
+)
+
+head = 1
+show_logits(
+    unembed(cache["blocks.0.attn.hook_result"][dataset_idx, :, head]),
+    dataset.toks[dataset_idx],
+    probs=False,
+    title=f"h0.{head} output unembed on dataset_idx {dataset_idx}"
+)
+
+#show_logits(
+#    unembed(model.blocks[0].attn.b_O.repeat(21, 1)),
+#    dataset.toks[dataset_idx],
+#    probs=False,
+#    title=f"attn b_O unembedded"
+#)
+
+show_logits(
+    unembed(cache['blocks.0.hook_resid_pre'][dataset_idx]),
+    dataset.toks[dataset_idx],
+    probs=False,
+    title=f"pre-attn residual unembedded on dataset idx {dataset_idx}"
+)
+# This is very interesting. The main feature of unembeddings the pre-attention residual stream is that
+# the logits for the previous correct token are strongly negative. So at each position s, the logits for
+# the token at s-1 are strongly negative. how??????
+# literally how can you do this without an attention layer.
+show_logits(
+    unembed(cache['blocks.0.hook_resid_pre'][dataset_idx] + cache['blocks.0.attn.hook_result'][dataset_idx, :, 0] + cache['blocks.0.attn.hook_result'][dataset_idx, :, 1] + model.blocks[0].attn.b_O),
+    dataset.toks[dataset_idx],
+    probs=False,
+    title=f"all together now"
+)
 #%%
 
-posembed_normed = model.W_E_pos / model.W_E_pos.norm(dim=-1, keepdim=True)
-imshow(einops.einsum(posembed_normed, posembed_normed, "voc1 d_model, voc2 d_model -> voc1 voc2"), title="Similarity matrix of posembeddings")
+pre = cache['blocks.0.hook_resid_pre']
+emb = cache['hook_embed']
+posemb = cache['hook_pos_embed']
+t.testing.assert_close(pre, emb + posemb)
 
 #%%
--- ideas.md
+++ ideas.md
@@ -1,4 +1,4 @@
-# try to make 'thinktoks' work.
+# model with invisible 'thinking' tokens trained via rl, with rewards based on supervised token accuracy
 - [thinktoks](https://githubcom/ekhadley/thinktokens) was a project I started but never saw results on due to training code being very complicated.
 - during ordinary pretraining, the model autoregressively outputs tokens on which we calculate crossentropy against the correct token for each sequence position.
 - the idea was to add a bunch of extra tokens to the models vocab.
@@ -15,7 +15,27 @@
 - The way chat models work is that they are originally trained to predict, then we set up the prompt so that their job is to predict the outputs of a character who happens to be a helpful AI assistantin conversation with a curious user. This can be done with just prompting but improved via fine tuning. The think out loud strategy allows the model to imitate the outputs of a human reasoning out loud, but there is evidence that the model is doing things somewhat different from what it appears. (see research on perturbing chains of thought and observing curiously unaffected final answers)
 - Doing rl on chain of thought is hard (but is what openai's o-models are doing and its clearly working), but doing rl on next-token prediction is easy.
 - I think it's plausible that this, or something like it, is actually what openai's o-models are doing behind the scenes. Having 'thinking' tokens whcih dont meaningfully translate to textual tokens could be why the o models output summaries of their thought process instead of the original tihng. but it could also be to hide whatever thinking strategies/thought processes they have trained the model to use.
-- There are endless variations to this theme. You could have a multihead model, where one thinks and one speaks, or you could expand the vocab of a pretrained model and fine tune it vis just the rl to use the invisible tokens, etc. (I would guess openai does this last one since pretraining via autoregression is very not parallelizable)
+- There are endless variations to this theme. You could have a multihead model, where one thinks and one speaks, or you could expand the vocab of a pretrained model and fine tune it vis just the rl to use the invisible tokens, etc. (I would guess openai does this last one since pretraining via autoregression is very not parallelizable) (actually since deepseek r1 is just doing thinking inside of <think> </think> tags, I would guess that's also what the o models do)
+- thought: it's easier to do alternating next token prediction, and think-token rl training rather than both simultaneously.
+
+# circuit mining via unsupervised component importance clustering
+- take some set of inputs and model predictions.
+- Create some set of metrics to establish which components of the model are important for a particular token prediction.
+- Do unsupervised clustering to find sets of sequences which use a similair subset of model components to make
+a prediction
+- Then zoom into some particular cluster and repeat, finding clusters of tightly related components within that data subset.
+- This would allow us to zoom in to particular component groups which specialize for performing certain tasks.
+- Assuming we are able to find nice identifiable clusters, this would show something akin to 'feature splitting' from
+the SAE literature, where we can zoom into activations of a particular feature and find sub-features.
+- Example of an interesting thing we might find: the model uses some sparse subset of components for doing math. We
+perform clustering on just math problems, and find one subset of the subset is responsible for doing subtraction, one
+for converting between currencies of different countries, one for calculating percentages, etc.
+- Could mine a model for all/a bunch of its circuits and pair them with strogn activating examples, like neuronpedia.
+- Then if one wanted to find the circuit responsible for some task, they would just have to produce a dataset which
+demonstrates the capability, and we could use the circuit-pedia to identify which cluster the dataset's examples
+are closest to.
+
+# simple idea: training encoder-decoder transformers to decompile program binaries
 
 # There are many methods of circuit discovery now. What do we do with the circuits
 - Has there been any work in actually using discovered circuits to 'fix' or contrain models?
@@ -34,7 +54,7 @@
 
 # general vein: mechinterp on reasoning models
 - for exmaple, agentic-type reasoning models have to soemtimes realize "this approach isnt working let me try something else". Can we discover the circuit that triggers this?
-- A reasoning model also has to choose when to stop thinking and start 'speaking'. Can the responsible circuit be discovered?   
+- A reasoning model also has to choose when to stop thinking and start 'speaking'. Can the responsible circuit be discovered? Can it be intervened to make a model think more/less than it normally would?
 
 # general vein: doing automated circuit discovery during pretraining. what changes over time?
 
